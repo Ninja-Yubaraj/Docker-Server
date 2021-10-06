3b7f3f3dfda65e146f8ea89468f89a4053659a93
Yubaraj Sarkar <yubarajsarkar2001@gmail.com>
2021-10-06 23:32:06 +0530

    added update_portainer.sh

---
+++ update_portainer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+portainer_pid=`docker ps | grep portainer-ce | awk '{print $1}'`
+portainer_name=`docker ps | grep portainer-ce | awk '{print $2}'`
+
+sudo docker stop $portainer_pid
+sudo docker rm $portainer_pid
+sudo docker rmi $portainer_name
+cd /home/docker/data/portainer
+sudo docker pull portainer/portainer-ce:latest
+sudo docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+cd ~
