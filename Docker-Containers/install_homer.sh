bf634ff1f67bda9ec9c6dda02bbfb30aed129c24
Yubaraj Sarkar <yubarajsarkar2001@gmail.com>
2021-10-16 20:17:19 +0530

    fixed some sudo stuff....

--- Docker-Containers/install_homer.sh
+++ Docker-Containers/install_homer.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 function error {
   echo -e "\\e[91m$1\\e[39m"
@@ -19,6 +19,6 @@ check_internet
 
 mkdir -p /home/docker/homer || error "Could not create /home/docker/homer"
 cd /home/docker/homer || error "Could not cd to /home/docker/homer"
-docker pull b4bz/homer || error "Could not pull b4bz/homer"
-docker run -d -p 8095:8080 -v /home/docker/homer/Config/Homer/assets:/www/assets --name="homer" --restart=always b4bz/homer:latest || error "Could not run homer"
+sudo docker pull b4bz/homer || error "Could not pull b4bz/homer"
+sudo docker run -d -p 8095:8080 -v /home/docker/homer/Config/Homer/assets:/www/assets --name="homer" --restart=always b4bz/homer:latest || error "Could not run homer"
 cd ~ || error "Could not cd to ~"
